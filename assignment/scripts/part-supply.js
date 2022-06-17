859c4a5a4566d7be31dc3b02cfbbac8910093066
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-06-17 10:21:42 -0500

    updates to 6-8

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -34,16 +34,39 @@ console.log(supplyChanges);
 //    - if the value is 0, log 'No Change.'
 //    - if the value is negative, format the log as 'Removed x parts.' 
 console.log('6. Showing supplyChanges...');
-
+for (let i=0; i<supplyChanges.length; i+=1) {
+    if (supplyChanges[i] > 0) {
+        console.log(`Added ${supplyChanges[i]} parts.`);
+    } else if (supplyChanges[i] === 0) {
+        console.log('No Change.');
+    } else if (supplyChanges[i] < 0) {
+        console.log(`Removed ${supplyChanges[i]} parts.`);
+    }
+}
 
 // STRETCH GOALS
 console.log('---  Stretch Goals  ---');
 // 7. Rewrite the `for` loop from #6 as a `for of` loop. 
 console.log('7. Showing supplyChanges with "for of" loop');
+for (partsNumber of supplyChanges) {
+    if (partsNumber > 0) {
+        console.log(`Added ${partsNumber} parts.`);
+    } else if (partsNumber === 0) {
+        console.log('No Change.');
+    } else if (partsNumber < 0) {
+        console.log(`Removed ${partsNumber} parts.`);
+    }
+}
 
 // 8. Write a loop to determine the total number of parts available by
 //    adding up all the numbers in the 'supplyChanges' array.
 console.log('8. Total supplies available is:');
+
+let partCount = 0;
+for (let i=0; i<supplyChanges.length; i+=1) {
+    partCount+=supplyChanges[i];
+}
+console.log(partCount);
 
 // 9. We have a large stash of parts in our warehouse that we 
 //    need to box up and get ready for shipment. 
